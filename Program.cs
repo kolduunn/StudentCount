b6e9e5610cd87fceaa8629d223b003086df57d8f
Пользователь <meowd098@gmail.com>
2025-03-11 18:15:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentCount
+{
+
+    internal class Program
+    {
+
+        static dynamic RemoveStudent(int id, List<Student> students)
+        {
+            var remainingStudents = from s in students
+                                    where s.Id != id
+                                    select s;
+
+            return remainingStudents;
+        }
+
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(1, "John"),
+                new Student(2, "Alice"),
+                new Student(3, "Bob")
+            };
+
+            var remainingStudents = RemoveStudent(2, students);
+
+            int count = 0;
+
+            foreach (var student in remainingStudents)
+            {
+                count++;
+            }
+            Console.WriteLine($"RemainingStudents: {count}");
+        }
+    }
+}
